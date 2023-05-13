53a8c71901f48bf3f8846440b5d0967d416866ce
Thai Hoang <110578242+ThaiHoang-newbie@users.noreply.github.com>
2023-05-13 14:11:35 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import './App.css';
 // import Fashion from './Components/States/Fashion';
 // import Form from './Components/States/Form';
 // import Score from './Components/States/Score';
-import Table_score from './Components/States/Table_score';
+// import Table_score from './Components/States/Table_score';
 // import Count from './Components/States/Hook/Count';
 // import Color from './Components/States/Hook/Color';
+// import Menu from './Components/States/Menu/Menu';
+import Render_API from './Components/Render_API/Render_API';
 
 function App() {
   return (
     <div>
-      {/* <Camera></Camera> */}
-      {/* <Fashion></Fashion> */}
-      {/* <Input_name></Input_name> */}
-      {/* <Form></Form> */}
-      {/* <Score></Score> */}
+      {/* <Camera></Camera>
+      <Fashion></Fashion>
+      <Input_name></Input_name>
+      <Form></Form>
+      <Score></Score>
       <Table_score></Table_score>
+      <Menu></Menu>
+      <Count></Count>
+      <Color></Color> */}
 
-      {/* <Count></Count> */}
-      {/* <Color></Color> */}
+
+      <Render_API></Render_API>
 
     </div>
 
